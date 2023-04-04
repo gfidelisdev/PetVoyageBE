6c0bc6cf112c4ee3950401e6429fbe5b6e480663
Guilherme Fidelis da Silva <109247627+gfidelisdev@users.noreply.github.com>
2023-04-04 06:39:25 -0300

    first

---
+++ app/Traits/MyUuid.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+
+use Ramsey\Uuid\Uuid;
+
+trait MyUuid
+{
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if ($model->getKey() == null) {
+                $model->setAttribute($model->getKeyName(), Uuid::uuid4()->toString());
+            }
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
